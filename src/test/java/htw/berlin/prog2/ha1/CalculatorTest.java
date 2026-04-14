027e2ca23a65c40db622cfc3570d40464caa3828
justin <justinmillerjustin27@gmail.com>
2026-04-14 23:13:09 +0200

    1. Commit für neuen grünen Test

--- src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -90,5 +90,24 @@ class CalculatorTest {
 
 
     //TODO hier weitere Tests erstellen
+
+    @Test
+    @DisplayName("should display result after adding 2 negative numbers")
+    void testNegativeSubtraction() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(3);
+        calc.pressNegativeKey();
+        calc.pressBinaryOperationKey("+");
+        calc.pressDigitKey(5);
+        calc.pressNegativeKey();
+        calc.pressEqualsKey();
+
+        String expected = "-8";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
 }
 
